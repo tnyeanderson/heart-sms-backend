86b89ad0eb9684febd9dc04f82b5839dcfaee49b
thomas <me@thomasanderson.cloud>
2020-12-14 05:37:35 -0500

    Cast booleans

--- db/connect.example.js
+++ db/connect.example.js
@@ -6,7 +6,15 @@ var connect = function () {
         port: 8000,
         user: 'test',
         password: 'testpassword',
-        database: 'heartsms'
+        database: 'heartsms',
+        typeCast: function (field, next) {
+            // Cast TINYINT(1) to boolean
+            if (field.type === 'TINY' && field.length === 1) {
+                return (field.string() === '1'); // 1 = true, 0 = false
+            } else {
+                return next();
+            }
+        }
     });
     
     return con;
