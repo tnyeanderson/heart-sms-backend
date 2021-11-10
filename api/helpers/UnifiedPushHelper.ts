c1d9a301f707784b57812cccc2d6ccb10c2f5e9c
thomas <me@thomasanderson.cloud>
2021-11-10 01:53:21 -0500

    cache tokens by account_id for performance

--- api/helpers/UnifiedPushHelper.ts
+++ api/helpers/UnifiedPushHelper.ts
@@ -1,14 +1,36 @@
 import db from '../db/query.js';
 
+class TokenCacheItem {
+	constructor(
+		public account_id: string,
+		public push_url: string,
+		public push_app_token: string
+	) { }
+}
+
 class UnifiedPushHelper {
-	async getMessagePushUrl(accountId: string) {
+	tokenCache = new Map<string, TokenCacheItem>();
+
+	async getPushConfig(accountId: string) {
 		const sql = `SELECT account_id, session_id, push_url, push_app_token FROM UnifiedPush
 			INNER JOIN SessionMap USING (account_id)
 			WHERE ${db.whereAccount(accountId)} LIMIT 1`;
 
 		const result = await db.query(sql);
 
-		return `https://${result[0].push_url}/message?token=${result[0].push_app_token}`
+		return new TokenCacheItem(result[0].session_id, result[0].push_url, result[0].push_app_token);
+	}
+
+	async getMessagePushUrl(accountId: string) {
+		let config = this.tokenCache.get(accountId);
+
+		if (!config) {
+			config = await this.getPushConfig(accountId);
+			// Add it to the cache
+			this.tokenCache.set(config.account_id, config);
+		}
+
+		return `https://${config.push_url}/message?token=${config.push_app_token}`;
 	}
 }
 
